a225d06e4c5714b98fd63aeafd6cb9412c80d3ff
Geetika gupta <geetika.gupta@knoldus.in>
2017-03-25 22:17:20 +0530

    Presto Example

--- examples/presto/src/main/java/com/facebook/carbondata/examples/PrestoExample.java
+++ examples/presto/src/main/java/com/facebook/carbondata/examples/PrestoExample.java
@@ -18,12 +18,14 @@
  */
 package com.facebook.carbondata.examples;
 
-import org.apache.carbondata.examples.util.ExampleUtils;
-import org.apache.carbondata.hadoop.CarbonInputFormat;
-import org.apache.carbondata.hadoop.CarbonProjection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.spark.sql.CarbonContext;
+import com.facebook.presto.carbondata.CarbondataPlugin;
+import com.facebook.presto.spi.connector.ConnectorFactory;
 
 /**
  * Created by knoldus on 22/3/17.
@@ -32,15 +34,70 @@ public class PrestoExample {
 
   public static void main(String args[]) {
 
-    CarbonContext cc = ExampleUtils.createCarbonContext("PrestoExample");
-    String path = ExampleUtils.writeSampleCarbonFile(cc, "carbon1", 1000);
-    CarbonProjection projection = new CarbonProjection();
-    projection.addColumn("c1");  // column c1
-    projection.addColumn("c3");  // column c3
-    Configuration conf = new Configuration();
-    CarbonInputFormat.setColumnProjection(conf, projection);
+    /*CarbonContext cc = ExampleUtils.createCarbonContext("PrestoExample");
+    String path = ExampleUtils.writeSampleCarbonFile(cc, "carbon1", 1000);*/
 
-    //val env = ExecutionEnvironment.getExecutionEnvironment
+    CarbondataPlugin x = new CarbondataPlugin();
+    Iterable<ConnectorFactory> y = x.getConnectorFactories();
+    final String JDBC_DRIVER = "com.facebook.presto.jdbc.PrestoDriver";
+    final String DB_URL = "jdbc:presto://19.168.2.188:8080/hive/default";
+
+    //  Database credentials
+    final String USER = "username";
+    final String PASS = "password";
+
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+      //STEP 2: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 3: Open a connection
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+      //conn.setCatalog("hive");
+
+
+      //STEP 4: Execute a query
+      stmt = conn.createStatement();
+      String sql;
+      sql = "create table test(a String)";
+      System.out.println("I m here---"+ conn.getSchema());
+      Boolean res = stmt.execute(sql);
+      System.out.println("Result==========="+ res);
+
+      //STEP 5: Extract data from result set
+      /*while(rs.next()){
+        //Retrieve by column name
+        int nationkey  = rs.getInt("n_nationkey");
+        String name = rs.getString("n_name");
+        int regionkey = rs.getInt("n_regionkey");
+        String comment = rs.getString("n_comment");
+
+        //Display values
+        System.out.println(String.format("%d, %s, %d, %s", nationkey, name, regionkey, comment));
+      }*///STEP 6: Clean-up environment
+     // rs.close();
+      stmt.close();
+      conn.close();
+    } catch (SQLException se) {
+      //Handle errors for JDBC
+      se.printStackTrace();
+    } catch (Exception e) {
+      //Handle errors for Class.forName
+      e.printStackTrace();
+    } finally {
+      //finally block used to close resources
+      try {
+        if (stmt != null) stmt.close();
+      } catch (SQLException se2) {
+      }
+      try {
+        if (conn != null) conn.close();
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
+    }
+    System.out.println("Goodbye!");
   }
-
 }
+
